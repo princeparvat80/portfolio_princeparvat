f1730adb913594079fb593c6de22f8b3698acd4a
Prince <princeparvat80@gmail.com>
2023-04-22 03:31:53 +0530

    css fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -52,7 +52,7 @@ const About = () => {
         Let's work together to bring your ideas to life!
       </motion.p>
 
-      <div className="mt-20 flex flex-wrap gap-10">
+      <div className="mt-20 flex gap-10">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}

0e588393b849565266b12ce96e22d5a62594c54f
Prince <princeparvat80@gmail.com>
2023-04-30 18:13:33 +0530

    tilt fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import Tilt from "react-tilt";
+import { Tilt } from 'react-tilt';
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -8,7 +8,7 @@ import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
-  // <Tilt className="xs:w-[250px] w-full">
+  <Tilt className="xs:w-[250px] w-full">
   <motion.div
     variants={fadeIn("right", "spring", index * 0.5, 0.75)}
     className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
@@ -30,7 +30,7 @@ const ServiceCard = ({ index, title, icon }) => (
       <a href="https://www.linkedin.com/in/prince-parwat-47308214b/" className="text-white text-[20px] font-bold text-center">{title}</a>
     </div>
   </motion.div>
-  // </Tilt>
+   </Tilt>
 );
 
 const About = () => {

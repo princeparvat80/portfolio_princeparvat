325447ae3948cb91fd5fba1624f919f056ad8059
Prince <princeparvat80@gmail.com>
2023-06-12 20:22:22 +0530

    Moved helpers to index

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import './HelperFun000937340'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
